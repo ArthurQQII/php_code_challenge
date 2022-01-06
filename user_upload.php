17ff6fb72495c97d38aabbbcc8b90aa813f54480
ArthurQQII <aurtherqi@gmail.com>
2022-01-06 23:47:32 +1000

    [feat]analyse file and  output formatted information
    1. read the csv file and validate the user information.

--- user_upload.php
+++ user_upload.php
@@ -37,17 +37,59 @@ function create_table($conn)
     }
 }
 
+function format_validate_user($users)
+{
+    $output = [];
+    $count = 0;
+    foreach ($users as $user) {
+        if (strlen($user['name']) == 0 || strlen($user['surname']) == 0) {
+            die("name and surname should not be null\n");
+        } else {
+            if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
+                fprintf(STDOUT, "email is not valid   %s\n", $user['email']);
+            } else {
+                $output[$count]['name'] = ucfirst($user['name']);
+                $output[$count]['surname'] = ucfirst($user['name']);
+                $output[$count++]['email'] = strtolower($user['email']);
+            }
+        }
+    }
+
+    foreach ($output as  $q) {
+        echo $q['name'] . "  " . $q['surname'] . "  " . $q['email'] . "\n";
+    }
+}
+
+function read_csv_file($fileName)
+{
+    $file = fopen($fileName, "r");
+    $users = [];
+    $userCount = 0;
+    $userData = fgetcsv($file);
+    while (($userData = fgetcsv($file)) !== false) {
+        if (count($userData) == 3) {
+            $users[$userCount++] = [
+                "name" => $userData[0],
+                "surname" => $userData[1],
+                "email" => $userData[2],
+            ];
+            //echo $users[$userCount - 1]['name'] . "  " . $users[$userCount - 1]['surname'] . "  " . $users[$userCount - 1]['email'] . "\n";
+        }
+    }
+    format_validate_user($users);
+}
 /**
  * main function
  */
 function main()
 {
-    $servername = "localhost";
-    $username = "root";
-    $password = "password";
-    $conn = connect_db($servername, $username, $password);
-    create_table($conn);
-    $conn->close();
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "password";
+    // $conn = connect_db($servername, $username, $password);
+    // create_table($conn);
+    // $conn->close();
+    read_csv_file('users.csv');
 }
 
 /**
